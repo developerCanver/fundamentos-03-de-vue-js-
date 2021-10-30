8377d4060bdee00fa8fc2ba991ddd56511f7f085
Carlos Ruiz Rivera <78513587+developerCanver@users.noreply.github.com>
2021-10-29 23:55:39 -0500

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2>Counter !!</h2>
+  <p>{{ counter }} <sup>2</sup> ={{ counter * counter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ counter * counter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ counter * counter  }}</p>
+
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+  <p>{{ counter }} <sup>2</sup> ={{ squareCounter  }}</p>
+
+
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{
+            counter:5
+        }
+    },
+    methods:{
+
+        getSquareValue(){
+            return 'Hola Mundo';
+        }
+    },
+    computed:{
+        //metodos compute se guardan en cahe
+        //solo se ejecuta solo una vez 
+        squareCounter(){
+            console.log('squareCounter')
+            return this.counter*this.counter;
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
